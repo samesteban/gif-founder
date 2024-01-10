64bd60b5c9aec149597b46dd9d3f5512822728af
s.bustos@want.cl <s.bustos@want.cl>
2024-01-10 12:32:12 -0300

    feat: add "last search" notation

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -6,9 +6,15 @@ const GifGrid = ({ category }) => {
 	const { images, isLoading } = useFetchGifs(category);
 
 	return (
-		<div key={category} className="p-8 odd:bg-stone-100 even:bg-stone-200">
-			<h2 className="mb-3 text-xl font-semibold">
-				{isLoading ? "Cargando..." : category}
+		<div
+			key={category}
+			className="p-8 odd:bg-stone-100 even:bg-stone-200 [&_.last-search]:first:inline-block"
+		>
+			<h2 className="mb-3 text-2xl font-semibold">
+				{isLoading ? "Cargando..." : category}{" "}
+				<span className="hidden text-sm font-light tracking-tighter last-search">
+					(last search)
+				</span>
 			</h2>
 			<div className="grid w-full grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
 				{images.map((image) => (
